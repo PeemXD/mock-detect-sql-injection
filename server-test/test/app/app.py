d2dbec974a8f04c23e6c383070a578c7bb14a376
Peem Auppati <104556421+PeemXD@users.noreply.github.com>
2023-02-19 22:11:31 +0700

    eiei

--- server-test/test/app/app.py
+++ server-test/test/app/app.py
@@ -1,22 +1,21 @@
-from flask import Flask,jsonify, request ,render_template
-import mysql.connector
-import re
+from flask import Flask, jsonify, request
 import joblib
+import mysql.connector
+from flask_cors import CORS
+import re
 
-app = Flask(__name__,template_folder='views')
+app = Flask(__name__)
+CORS(app)
 
+# Connect to MySQL
+connection = mysql.connector.connect(user='root',
+                                     password='root',
+                                     host='db',
+                                     port='3306',
+                                     database='mockInjection')
 
+cursor = connection.cursor()
 
-
-connection = mysql.connector.connect(user = 'root',
-        password = 'root',
-        host= 'db',
-        port= '3306',
-        database= 'mockInjection')
-
-@app.route('/')
-def index2():
-    return render_template('login.html')
 
 def haveBlackListWord(input):
     blacklist = [
@@ -62,6 +61,8 @@ def haveBlackListWordInFile(input):
         "database",
         "table",
         "column",
+        "=",
+        "--",
     ]
     regex = re.compile(rf"\b({'|'.join(blacklist)})\b", re.IGNORECASE)
     return regex.search(input) is not None
@@ -69,29 +70,32 @@ def haveBlackListWordInFile(input):
 
 def validPattern(input):
     pattern = r'^[ก-๏a-zA-Z\d0-9]+$'
-    return not re.match(pattern, input)
+    return re.match(pattern, input) is not None
+
 
 def vectorizer(input):
-    vector= joblib.load('./ML/vectorizerv4')
+    vector = joblib.load('./ML/vectorizerv4')
     input = [input]
     input_for_ML = vector.transform(input).toarray()
     return input_for_ML
 
+
 def randomForest(input):
     model = joblib.load('./ML/random-forestv3')
-    result=model.predict(input)
+    result = model.predict(input)
     return result[0]
+
 
 def SVM(input):
     model = joblib.load('./ML/SVM')
-    result=model.predict(input)
+    result = model.predict(input)
     return result[0]
+
 
 def NaiveBay(input):
     model = joblib.load('./ML/Naive Bayv2')
-    result=model.predict(input)
+    result = model.predict(input)
     return result[0]
-
 
 
 @app.route("/", methods=['GET'])
@@ -104,32 +108,41 @@ def login():
     data = request.json.get('data')
     username = data.get('username')
     password = data.get('password')
+    # print(data)
+    # print(username, password)
+    if (haveBlackListWordInFile(username) or haveBlackListWordInFile(password)):
+        return jsonify({'success': "This injection"})
 
-    cursor = connection.cursor()
-    cursor.execute(
-        "SELECT * FROM users WHERE username = %s AND password = %s", (username, password))
-    result = cursor.fetchone()
+    sql = f'SELECT * FROM users WHERE username = "{username}" AND password = "{password}"'
+    print(sql)
+    cursor.execute(sql)
+    result = cursor.fetchall()
 
     if not result:
-        return jsonify({'success': False}), 401
+        return jsonify({'success': False})
     else:
         return jsonify({'success': True})
 
-    
 
-@app.route("/employee", methods=['GET'])
+@ app.route("/employee", methods=['GET'])
 def employee():
     id = request.args.get('id')
 
-    cursor = connection.cursor()
+    # if (haveBlackListWordInFile(id)):
+    #     return jsonify({'success': "This injection"})
+    print(randomForest(vectorizer(input)))
+
     if not id:
-        cursor.execute(
-            "SELECT employee_id, prefixname, fname, lname, nickname, email, tel FROM employee")
+        sql = f'SELECT employee_id, prefixname, fname, lname, nickname, email, tel FROM employee'
+        print(sql)
+        cursor.execute(sql)
         result = cursor.fetchall()
     else:
-        cursor.execute(
-            "SELECT employee_id, prefixname, fname, lname, nickname, email, tel FROM employee WHERE employee_id = %s", (id,))
-        result = cursor.fetchone()
+        sql = f'SELECT employee_id, prefixname, fname, lname, nickname, email, tel FROM employee WHERE employee_id = "{id}"'
+        print(sql)
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        print(result)
 
     if not result:
         return jsonify({'success': False}), 401
@@ -137,16 +150,19 @@ def employee():
         return jsonify(result)
 
 
-@app.route("/employee/uploadFile", methods=['POST'])
+@ app.route("/employee/uploadFile", methods=['POST'])
 def employee_upload_file():
     data = request.json.get('fileText')
-    cursor = connection.cursor()
-    cursor.execute(data)
+    sql = f'{data}'
+
+    if (haveBlackListWordInFile(sql)):
+        return jsonify({'success': "This injection"})
+
+    cursor.execute(sql)
     connection.commit()
     return jsonify({'success': True})
 
 
-
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0')
-
+    app.run(host='0.0.0.0', debug=True)
+    # print("Server started on http://localhost:3000")
