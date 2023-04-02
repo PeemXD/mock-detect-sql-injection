14fa89491c35cd7cc171ea4a5a3d75788354a737
Peem Auppati <104556421+PeemXD@users.noreply.github.com>
2023-04-02 21:31:45 +0700

    +

--- index.py
+++ index.py
@@ -1,3 +1,4 @@
+import time
 from flask import Flask, jsonify, request
 import joblib
 import mysql.connector
@@ -6,7 +7,6 @@ import re
 import threading
 
 mutex = threading.Lock()
-count = 0
 
 app = Flask(__name__)
 CORS(app)
@@ -23,6 +23,15 @@ connection = mysql.connector.connect(
     database="mockinjection",
 )
 cursor = connection.cursor()
+
+count = 0
+ruleBaseCount = 0
+NaiveBayCount = 0
+randomforestCount = 0
+knnCount = 0
+NaiveBayCount_smote = 0
+randomforestCount_smote = 0
+knnCount_smote = 0
 
 
 def haveBlackListWord(input):
@@ -81,27 +90,65 @@ def validPattern(input):
     return re.match(pattern, input) is not None
 
 
-def vectorizer(input):
-    vector = joblib.load('./ML/vectorizerv3')
+def vectorizerOld(input):
+    vector = joblib.load('ML/vectorizerv3')
     input = [input]
     input_for_ML = vector.transform(input).toarray()
     return input_for_ML
 
 
+def randomForestOld(input):
+    model = joblib.load('ML/random-forestv3')
+    result = model.predict(input)
+    return result[0]
+
+
+def vectorizer(input):
+    vector = joblib.load('ML-last/vectorizerFinal')
+    input = [input]
+    input_for_ML = vector.transform(input).toarray()
+    return input_for_ML
+
+
+def naiveBay(input):
+    model = joblib.load('ML-last/NaiveBay')
+    result = model.predict(input)
+    return result[0]
+
+
 def randomForest(input):
-    model = joblib.load('./ML/random-forestv3')
+    model = joblib.load('ML-last/randomforest')
     result = model.predict(input)
     return result[0]
 
 
-def SVM(input):
-    model = joblib.load('./ML/SVM')
+def KNN(input):
+    model = joblib.load('ML-last/knn')
     result = model.predict(input)
     return result[0]
 
 
-def NaiveBay(input):
-    model = joblib.load('./ML/Naive Bayv2')
+def vectorizerSmote(input):
+    vector = joblib.load('ML-smote-v2/vectorizer_smotev2')
+    input = [input]
+    input_for_ML = vector.transform(input).toarray()
+    return input_for_ML
+
+
+def naiveBaySmote(input):
+    model = joblib.load('ML-smote-v2/NaiveBay_SMOTEv2')
+    result = model.predict(input)
+    return result[0]
+
+
+def randomForestSmote(input):
+    model = joblib.load('ML-smote-v2/randomforest_SMOTEv2')
+    result = model.predict(input)
+    return result[0]
+
+
+def KNNSmote(input):
+    model = joblib.load('ML-smote-v2/knn_SMOTEv2')
     result = model.predict(input)
     return result[0]
 
@@ -117,17 +164,8 @@ def login():
     username = data.get('username')
     password = data.get('password')
 
-    if (haveBlackListWordInFile(username) or haveBlackListWordInFile(password)):
-        mutex.acquire()
-        count += 1
-        mutex.release()
-        return jsonify({'success': "This injection"})
-    else:
-        return jsonify({'success': "This is not injection"})
-
-    # outPut1 = vectorizer(username)
-    # outPut2 = randomForest(outPut1)
-    # if outPut2:
+    #! RuleBase
+    # if (haveBlackListWordInFile(username) or haveBlackListWordInFile(password)):
     #     mutex.acquire()
     #     count += 1
     #     mutex.release()
@@ -135,6 +173,44 @@ def login():
     # else:
     #     return jsonify({'success': "This is not injection"})
 
+    #! ML
+    # outPut1A = vectorizer(username)
+    # outPut1B = vectorizer(password)
+    # outPut2A = naiveBay(outPut1A)
+    # outPut2B = naiveBay(outPut1B)
+    # # outPut2A = randomForest(outPut1A)
+    # # outPut2B = randomForest(outPut1B)
+    # # outPut2A = KNN(outPut1A)
+    # # outPut2B = KNN(outPut1B)
+    # if outPut2A or outPut2B:
+    #     mutex.acquire()
+    #     count += 1
+    #     mutex.release()
+    #     return jsonify({'success': "This injection"})
+    # else:
+    #     return jsonify({'success': "This is not injection"})
+
+    #! test ML
+    outPut1A = vectorizer(username)
+    outPut1B = vectorizer(password)
+    outPut2A1 = naiveBay(outPut1A)
+    outPut2A2 = naiveBay(outPut1B)
+    outPut2B1 = randomForest(outPut1A)
+    outPut2B2 = randomForest(outPut1B)
+    outPut2C1 = KNN(outPut1A)
+    outPut2C2 = KNN(outPut1B)
+
+    global NaiveBayCount, randomforestCount, knnCount, count
+    if outPut2A1 or outPut2A2:
+        NaiveBayCount += 1
+    if outPut2B1 or outPut2B2:
+        randomforestCount += 1
+    if outPut2C1 or outPut2C2:
+        knnCount += 1
+    count += 1
+    return jsonify({'success': True})
+
+    #! core api
     # sql = f'SELECT * FROM users WHERE username = "{username}" AND password = "{password}"'
     # print(sql)
     # cursor.execute(sql)
@@ -149,27 +225,60 @@ def login():
 @ app.route("/employee", methods=['GET'])
 def employee():
     id = request.args.get('id')
-    # print(id)
+    print(id)
+    #! test
+    # ? RuleBase
+    outPutRuleBase = haveBlackListWord(id)
 
-    if (haveBlackListWordInFile(id)):
+    # ? No smote
+    outPutV = vectorizer(id)
+    outPutVOld = vectorizerOld(id)
+    outPutNB = naiveBay(outPutV)
+    outPutRF = randomForestOld(outPutVOld)
+    outPutKNN = KNN(outPutV)
+
+    # ? have smote
+    outPutVSmote = vectorizerSmote(id)
+    outPutNBSmote = naiveBaySmote(outPutVSmote)
+    outPutRFSmote = randomForestSmote(outPutVSmote)
+    outPutKNNSmote = KNNSmote(outPutVSmote)
+
+    global count,  ruleBaseCount, NaiveBayCount, randomforestCount, knnCount, NaiveBayCount_smote, randomforestCount_smote, knnCount_smote
+    count += 1
+    while True:  # wait until all ml success
+        if outPutNB is not None and outPutRF is not None and outPutKNN is not None:
+            break
+        time.sleep(0.1)  # wait for 100 milliseconds before checking again
+    # print(count, outPut2A, outPut2B, outPut2C)
+    if (outPutRuleBase):
         # mutex.acquire()
-        global count
-        count += 1
+        ruleBaseCount += 1
         # mutex.release()
-        return jsonify({'success': "This injection"})
-    else:
-        return jsonify({'success': "This is not injection"})
-    # outPut1 = vectorizer(id)
-    # outPut2 = randomForest(outPut1)
-    # if outPut2:
+    if outPutNB:
+        NaiveBayCount += 1
+    if outPutRF:
+        randomforestCount += 1
+    if outPutKNN:
+        knnCount += 1
+    if outPutNBSmote:
+        NaiveBayCount_smote += 1
+    if outPutRFSmote:
+        randomforestCount_smote += 1
+    if outPutKNNSmote:
+        knnCount_smote += 1
+    return jsonify({'success': True})
+
+    #! RuleBase
+    # if (haveBlackListWordInFile(id)):
+    #     # mutex.acquire()
     #     global count
-    #     mutex.acquire()
     #     count += 1
-    #     mutex.release()
+    #     # mutex.release()
     #     return jsonify({'success': "This injection"})
     # else:
     #     return jsonify({'success': "This is not injection"})
 
+    #! core api
     # if not id:
     #     sql = f'SELECT employee_id, prefixname, fname, lname, nickname, email, tel FROM employee'
     #     print(sql)
@@ -203,10 +312,20 @@ def employee_upload_file():
 
 @ app.route("/count", methods=['GET'])
 def getCount():
-    return jsonify({'success': True, 'count': count})
+    global count,  ruleBaseCount, NaiveBayCount, randomforestCount, knnCount, NaiveBayCount_smote, randomforestCount_smote, knnCount_smote
+    result = jsonify({'total': count,
+                      'RuleBase': '% .4f' % (ruleBaseCount/count),
+                      'NaiveBay': '% .4f' % (NaiveBayCount/count),
+                      'randomforest': '% .4f' % (randomforestCount/count),
+                      'knn': '% .4f' % (knnCount/count),
+                      'NaiveBay_smote': '% .4f' % (NaiveBayCount_smote/count),
+                      'randomforest_smote': '% .4f' % (randomforestCount_smote/count),
+                      'knn_smote': '% .4f' % (knnCount_smote/count), })
+    # NaiveBayCount, randomforestCount, knnCount, count = 0
+    return result
 
 
 if __name__ == "__main__":
     # app.run(host='0.0.0.0', debug=True)
-    app.run(host='localhost', port=3000)
     print("Server started on http://localhost:3000")
+    app.run(host='localhost', port=3000)
